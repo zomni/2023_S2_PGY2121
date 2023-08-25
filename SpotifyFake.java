901cdea7f989e068d220ca4299d32502375d684c
zomni <88544008+zomni@users.noreply.github.com>
2023-08-24 20:39:37 -0400

    Add files via upload

---
+++ SpotifyFake.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package cl.duocuc.pgy2121.spotifyfake;
+//comentario
+/**
+ *
+ * @author cetecom
+ */
+public class SpotifyFake {
+    public static void main(String[] args) {
+        char sexo = 'M';
+            String nombre = "Paolo";
+        boolean isValid = false;
+            byte bytes;
+        int edad = 21;
+        //fijar en la letra al final del valor
+            float decimal = 3.14f;
+            double doble = 3.10d;
+            long numeroLargo = 32423453456756l;
+            short numeroChiquito = 34;
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        //sout + tab
+        System.out.println("Activo: " + isValid);
+    }
+ }
